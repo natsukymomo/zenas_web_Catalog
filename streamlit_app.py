5b72029451560ff297d58867c1093766fc34d6b4
Momo <85818789+natsukymomo@users.noreply.github.com>
2022-09-07 14:14:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,14 @@ streamlit.text(my_data_row)
 streamlit.title("Zena's Amazing Athleisure Catalog")
 #----search bar
 my_cur.execute("select color_or_style from catalog_for_website")
-/*
-my_catalog = my_cur.fetchall()
-df = pandas.DataFrame(my_catalog)
+
+
+#my_catalog = my_cur.fetchall()
+#df = pandas.DataFrame(my_catalog)
 #streamlit.write(df)
 #color_list = df[0].values.tolist()
 #print(color_list)
 
 #option = streamlit.selectbox('Pick a sweatsuit color or style', list(color_list))
 
-*/
+
