9b874557fd22e1776acd080b061ec3a80a8a1ce2
Momo <85818789+natsukymomo@users.noreply.github.com>
2022-09-07 14:25:12 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,23 @@ import pandas
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor() 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:") 
-streamlit.text(my_data_row)
 
 streamlit.title("Zena's Amazing Athleisure Catalog")
 #----search bar
 my_cur.execute("select color_or_style from catalog_for_website")
 
-
 my_catalog = my_cur.fetchall()
 df = pandas.DataFrame(my_catalog)
 streamlit.write(df)
 color_list = df[0].values.tolist()
-print(color_list)
+#print(color_list)
 
 option = streamlit.selectbox('Pick a sweatsuit color or style', list(color_list))
+
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = option ;") 
+
+df2 = my_cur.fetchone()
 
 
